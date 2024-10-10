595e7000ee10464d6a3607dbf652031306b970ca
OmarOmar93 <omartemhosma7@gmail.com>
2024-10-10 22:50:25 +0300

    1.0.2 - Patching 4

--- BroadcastSystem/src/main/java/BroadCast/Enabler.java
+++ BroadcastSystem/src/main/java/BroadCast/Enabler.java
@@ -9,10 +9,18 @@ public class Enabler {
     public static void onEnable() {
         new WorldCasterConfig();
         final WCA api = new WCA();
-        final Addon main = api.createWCAddon("WorldCaster",
-                "OmarOmar93",
-                "Standalone Broadcast Add-on for WorldChatter!"
-                , "WCaster", "1.0.2","https://raw.githubusercontent.com/OmarOmar93/WCVersion/refs/heads/main/worldCasterVersion",102);
+        Addon main;
+        try {
+            main = api.createWCAddon("WorldCaster",
+                    "OmarOmar93",
+                    "Standalone Broadcast Add-on for WorldChatter!"
+                    , "WCaster", "1.0.2", "https://raw.githubusercontent.com/OmarOmar93/WCVersion/refs/heads/main/worldCasterVersion", 102);
+        } catch (Exception ignored) {
+            main = api.createWCAddon("WorldCaster",
+                    "OmarOmar93",
+                    "Standalone Broadcast Add-on for WorldChatter!"
+                    , "WCaster", "1.0.2");
+        }
         api.addListener(main, new events());
     }
 }
